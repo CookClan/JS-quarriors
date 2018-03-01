b0e20101b71fd4a81d30b71183c310fb757edf0e
Allen Cook <alcook16@gmail.com>
2018-03-01 00:07:47 -0500

    Upload Tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-junit-reporter'),
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -28,6 +29,15 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    junitReporter: {
+      outputDir: 'testResults', // results will be saved as $outputDir/$browserName.xml 
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile 
+      suite: '', // suite will become the package name attribute in xml testsuite element 
+      useBrowserName: true, // add browser name to report and classes names 
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element 
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element 
+      properties: {} // key value pair of properties to add to the <properties> section of the report 
+    }
   });
 };
