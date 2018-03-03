05bae9a76b1ab29201cd612a9cd07ed1cae0526b
Phillip Cook <phyxl01@gmail.com>
2018-03-02 22:15:22 -0600

    adding a link eventually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
