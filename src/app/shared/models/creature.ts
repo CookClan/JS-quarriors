94c9d76d48e7965dbd560bab7e99a5abe013ec8b
Allen Cook <alcook16@gmail.com>
2018-03-06 01:02:17 -0500

    Update creature.ts

--- src/app/shared/models/creature.ts
+++ src/app/shared/models/creature.ts
@@ -4,17 +4,18 @@ import { Spell } from './spell';
 /**
  * Holds the data about a Summoned creature
 */
-export class Creature { //might need to conolidate spells and creatures as one class "Rolled"
-    //this can either be another rolled die or a direct reference against an attached ability, would need a reference back to the original type either way.
-    private attachments: Rolled[]; 
+export class Creature { //might need to conolidate spells and creatures as one class or interface "Rolled"
+  //the skill that generates from a spell being cast with a reference to the original rolled type and side
+    private attachments: Attachment[]; 
     public constructor(private type: Type, private side: number) {}
     public diceSide(): DiceSide {
         return this.type.class.diceSides[this.side]; // determines what is displayed
+      //burst,icon,level
     }
     public get attack(): number {
-        return this.diceSide().attack; // will be extended
+        return this.diceSide().attack; // determines attack to be displayed
     }
     public get defense(): number {
-        return this.diceSide().defense; // will be extended
+        return this.diceSide().defense; // determines defense to be displayed
     }
 }
