a72176a5613d107027ed213ca297b4f4532d91d5
Allen Cook <alcook16@gmail.com>
2018-03-06 00:49:53 -0500

    Update player.ts

--- src/app/shared/models/player.ts
+++ src/app/shared/models/player.ts
@@ -7,7 +7,8 @@ import { Spell } from './spell';
 export class Player {
     private glory: number;
     private bag: Type[];
-    private ready: Rolled[]; //will be the result of the side rolled, and the type the die is bound to
+    private active: Rolled[];//will be the result of the side rolled, and the type the die is bound to
+    private ready: Rolled[]; 
     private spent: Rolled[];
     private used: Rolled[]; //we don't turn into types again till we get bagged, incase an ability brings the die back it has to recall the state it was in when it was removed from ready
 }
