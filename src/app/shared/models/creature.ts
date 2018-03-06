fe8c74fa5fc4dd9ed1e2ac7311bc7cf7a69b7193
Allen Cook <alcook16@gmail.com>
2018-03-06 00:54:57 -0500

    comments

--- src/app/shared/models/creature.ts
+++ src/app/shared/models/creature.ts
@@ -4,8 +4,9 @@ import { Spell } from './spell';
 /**
  * Holds the data about a Summoned creature
 */
-export class Creature {
-    private spells: Spell[]; //might be better to have this be the same class we use for bursts
+export class Creature { //might need to conolidate spells and creatures as one class "Rolled"
+    //this can either be another rolled die or a direct reference against an attached ability, would need a reference back to the original type either way.
+    private attachments: Rolled[]; 
     public constructor(private type: Type, private side: number) {}
     public diceSide(): DiceSide {
         return this.type.class.diceSides[this.side]; // determines what is displayed
