836bcbaaff32e9e4cbe0ec854a54113cfc376c2c
Phillip Cook <phyxl01@gmail.com>
2018-06-19 21:08:42 -0500

    lint the files again

--- src/app/shared/models/wilds.ts
+++ src/app/shared/models/wilds.ts
@@ -1,12 +1,12 @@
 import { Type } from '../models/type';
 import * as TypeLib from '../models/typeLib';
 
-const wildsTypes: [string, number][] = [['Basic',3], ['Spell',3], ['Creature', 7]];
+const wildsTypes: [string, number][] = [['Basic', 3], ['Spell', 3], ['Creature', 7]];
 const basicKeys: string[] = ['quiddity', 'assistant', 'portal'];
 
 export class Wilds {
   constructor() {
-    for (let section of wildsTypes){
+    for (const section of wildsTypes) {
       const sectionName = section[0];
       const sectionCount = section[1];
       this.wilds.concat(TypeLib.getRandomType(sectionName, sectionCount));
